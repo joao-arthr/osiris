98ba25ee29e7bce75e46fe328eb213b8eaebe152
joao-arthr <jao.arthurag@gmail.com>
2023-10-24 10:32:07 -0300

    feat: entity Receita added

---
+++ src/main/java/com/papaya/osiris/entity/Receita.java
@@ -0,0 +1,25 @@
+package com.papaya.osiris.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "receitas")
+public class Receita {
+    @Id
+    private ObjectId id;
+    private String nome;
+    private List<Panc> pancs;
+    private List<String> ingredientes;
+    private List<String> preparo;
+    private Integer likes;
+    private String imagem;
+}
