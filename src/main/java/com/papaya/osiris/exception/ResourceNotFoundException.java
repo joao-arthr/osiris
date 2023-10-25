b90e52e14010b81e877d5dee24936e5febbc8205
joao-arthr <jao.arthurag@gmail.com>
2023-10-25 12:55:41 -0300

    feat: ResourceNotFoundException adicionada

---
+++ src/main/java/com/papaya/osiris/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.papaya.osiris.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
