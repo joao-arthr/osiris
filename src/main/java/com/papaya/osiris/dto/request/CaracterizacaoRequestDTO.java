a03824243a2babd6447fbf986ceb5f6d8b7b0127
João Arthur <jao.arthurag@gmail.com>
2024-05-13 22:03:31 -0300

    feat: DTO da AI criados

---
+++ src/main/java/com/papaya/osiris/dto/request/CaracterizacaoRequestDTO.java
@@ -0,0 +1,6 @@
+package com.papaya.osiris.dto.request;
+
+import java.math.BigDecimal;
+
+public record CaracterizacaoRequestDTO(String classe, BigDecimal acuracia) {
+}
---
+++ src/main/java/com/papaya/osiris/dto/response/CaracterizacaoResponseDTO.java
@@ -0,0 +1,6 @@
+package com.papaya.osiris.dto.response;
+
+import java.math.BigDecimal;
+
+public record CaracterizacaoResponseDTO(String classe, BigDecimal acuracia) {
+}
